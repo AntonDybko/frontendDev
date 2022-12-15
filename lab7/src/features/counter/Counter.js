154607f1d0bd56a5fa40c1a2ddc6885393701614
adybko <dybko.anton11@ukr.net>
2022-12-15 22:15:03 +0100

    startredux

--- lab7/src/features/counter/Counter.js
+++ lab7/src/features/counter/Counter.js
@@ -1,67 +1,40 @@
 import React, { useState } from 'react';
 import { useSelector, useDispatch } from 'react-redux';
 import {
-  decrement,
-  increment,
+  getGames,
+  addToState,
   incrementByAmount,
   incrementAsync,
   incrementIfOdd,
   selectCount,
 } from './counterSlice';
-import styles from './Counter.module.css';
+import {
+  BrowserRouter as Router,
+  Routes,
+  Route,
+  Link
+} from "react-router-dom";
+import CompGameForm from '../../components/CompGameForm';
 
 export function Counter() {
-  const count = useSelector(selectCount);
+  const games = useSelector(getGames);
   const dispatch = useDispatch();
-  const [incrementAmount, setIncrementAmount] = useState('2');
+  //const [incrementAmount, setIncrementAmount] = useState('2');
+  //const [games, setGames] = useState([]);
 
-  const incrementValue = Number(incrementAmount) || 0;
+  //const incrementValue = Number(incrementAmount) || 0;
 
   return (
     <div>
-      <div className={styles.row}>
-        <button
-          className={styles.button}
-          aria-label="Decrement value"
-          onClick={() => dispatch(decrement())}
-        >
-          -
-        </button>
-        <span className={styles.value}>{count}</span>
-        <button
-          className={styles.button}
-          aria-label="Increment value"
-          onClick={() => dispatch(increment())}
-        >
-          +
-        </button>
-      </div>
-      <div className={styles.row}>
-        <input
-          className={styles.textbox}
-          aria-label="Set increment amount"
-          value={incrementAmount}
-          onChange={(e) => setIncrementAmount(e.target.value)}
-        />
-        <button
-          className={styles.button}
-          onClick={() => dispatch(incrementByAmount(incrementValue))}
-        >
-          Add Amount
-        </button>
-        <button
-          className={styles.asyncButton}
-          onClick={() => dispatch(incrementAsync(incrementValue))}
-        >
-          Add Async
-        </button>
-        <button
-          className={styles.button}
-          onClick={() => dispatch(incrementIfOdd(incrementValue))}
-        >
-          Add If Odd
-        </button>
-      </div>
+      <CompGameForm></CompGameForm>
+      <ul>
+        {games.map(game => (
+            <li key={game.id}>
+                <div>Name: {game.name}</div>
+                <div>Price: {game.price}</div>
+            </li>
+        ))}
+      </ul>
     </div>
   );
 }
